9c642fe965c3c2e3c1d02effb732f387d363c924
budigevenkataswamy <40264601+budigevenkataswamy@users.noreply.github.com>
2018-11-21 18:53:24 +0530

    Add files via upload

---
+++ com/core/practice/Abc.java
@@ -0,0 +1,11 @@
+package com.core.practice;
+
+public class Abc {
+	
+	public Abc() {}
+	
+	  public Abc(int i) {
+		  System.out.println("In a construtcor");
+	  }
+
+}
---
+++ com/core/practice/Construtors.java
@@ -0,0 +1,11 @@
+package com.core.practice;
+
+public class Construtors {
+
+	public static void main(String[] args) {
+		Abc obj=new Abc();
+		
+	}
+	
+	 
+}
